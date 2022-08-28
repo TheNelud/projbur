9a3d4a526dce67c4e7ad050916f7ca95f172003e
TheNelud <riwok@bk.ru>
2022-08-28 12:52:01 +0300

    работал с шаблоном техничекого обслуживания, стили , адаптивность

--- maintenance_app/urls.py
+++ maintenance_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.maintenance, name = "maintenance"),
+    # path('', views.pdf, name='pdf'),
     
 ]
--- maintenance_app/views.py
+++ maintenance_app/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from .models import *
+from django.http import FileResponse, Http404, HttpResponse
+import os
+
+# def pdf(request):
+#     with open('static/maintenance_app/pdf/keys.pdf', 'rb') as pdf:
+#         response = HttpResponse(pdf.read(),content_type='application/pdf')
+#         response['Content-Disposition'] = 'filename=keys.pdf'
+#         return response
+    # try:
+    #     filepath = os.path.join('static/maintenance_app/pdf', 'keys.pdf')
+    #     return FileResponse(open(filepath, 'rb'), content_type='application/pdf')
+    # except FileNotFoundError:
+    #     raise Http404('not found')
+
 # Create your views here.
 def maintenance(request):
     data_to_repairs = ToRepairs.objects.all()
